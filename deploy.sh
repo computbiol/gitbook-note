c113595b32fb373f18425bdb8d3a7d448e3e23df
computbiol <48988788+computbiol@users.noreply.github.com>
2019-08-13 20:15:23 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 # 设置远程仓库的地址
-remote_url=git@github.com:computbiol/gitbook-note.git
+#remote_url=git@github.com:computbiol/gitbook-note.git
+#git remote add origin $remote_url
 # 获取当前时间
-cur_date="`date +%Y-%m-%d-%H:%M:%S`" 
-# 生成_book文件
+#cur_date="`date +%Y-%m-%d-%H:%M:%S`" 
+
+# 生成_book文件并托管到github
 gitbook build
-#
-rm -rf .deploy_git/* | egrep .deploy_git/.git
-if [ ! -d ".deploy_git/" ];then
-	cp -R _book/ .deploy_git/
-else
-	cp -R _book/* .deploy_git/
-fi
-cd .deploy_git/
-git init
-git remote add origin $remote_url
-git checkout -b gh-pages
+cd gh-pages/
+rm -rf `ls | grep -v README.md`
+cd ../_book/
+cp -r `ls | grep -v deploy.sh` ../gh-pages/
+cd ../gh-pages/
 git add -A
-git commit -m $cur_date
-git push -f origin gh-pages
+git commit -m "Update"
+git push origin gh-pages
